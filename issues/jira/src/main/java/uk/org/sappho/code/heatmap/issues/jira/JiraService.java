077aaa863c5a7919d86ff386672d054eadef8782
Andrew Heald <andrew@heald.co.uk>
2010-11-29 16:20:36 +0000

    CHM-19: Map fix versions to releases and start reportable logging of warnings.

--- issues/jira/src/main/java/uk/org/sappho/code/heatmap/issues/jira/JiraService.java
+++ issues/jira/src/main/java/uk/org/sappho/code/heatmap/issues/jira/JiraService.java
@@ -2,19 +2,23 @@ package uk.org.sappho.code.heatmap.issues.jira;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.log4j.Logger;
 
 import com.atlassian.jira.rpc.soap.client.RemoteIssue;
+import com.atlassian.jira.rpc.soap.client.RemoteVersion;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
 import uk.org.sappho.code.heatmap.config.Configuration;
+import uk.org.sappho.code.heatmap.config.ConfigurationException;
 import uk.org.sappho.code.heatmap.issues.IssueManagement;
 import uk.org.sappho.code.heatmap.issues.IssueManagementException;
 import uk.org.sappho.code.heatmap.issues.IssueWrapper;
+import uk.org.sappho.code.heatmap.warnings.Warnings;
 import uk.org.sappho.jira4j.soap.JiraSoapService;
 
 @Singleton
@@ -22,16 +26,20 @@ public class JiraService implements IssueManagement {
 
     protected JiraSoapService jiraSoapService = null;
     protected Map<String, IssueWrapper> allowedIssues = new HashMap<String, IssueWrapper>();
+    protected Map<String, String> releases = new HashMap<String, String>();
     protected Map<String, String> issueTypes = new HashMap<String, String>();
     protected Map<String, Integer> issueTypeWeightMultipliers = new HashMap<String, Integer>();
+    protected Warnings warnings;
     protected Configuration config;
     protected static final Pattern SIMPLE_JIRA_REGEX = Pattern.compile("^([a-zA-Z]{2,}-\\d+):.*$");
     private static final Logger LOG = Logger.getLogger(JiraService.class);
+    protected static final String ISSUE_FIELDS = "Issue fields";
 
     @Inject
-    public JiraService(Configuration config) throws IssueManagementException {
+    public JiraService(Warnings warnings, Configuration config) throws IssueManagementException {
 
         LOG.info("Using Jira issue management plugin");
+        this.warnings = warnings;
         this.config = config;
         connect();
         getAllowedIssues();
@@ -76,7 +84,7 @@ public class JiraService implements IssueManagement {
                         jiraSoapService.getToken(), "parent = " + issueKey, 200);
                 for (RemoteIssue subTask : subTasks) {
                     String subTaskKey = subTask.getKey();
-                    LOG.info("Mapping " + subTaskKey + " to parent issue " + issueKey);
+                    warnings.add("Issue subtask mapping", subTaskKey + " --> " + issueKey);
                     if (mappedRemoteIssues.get(subTaskKey) == null) {
                         mappedRemoteIssues.put(subTaskKey, subTask);
                     }
@@ -100,11 +108,32 @@ public class JiraService implements IssueManagement {
 
     protected IssueWrapper createIssueWrapper(RemoteIssue issue, String subTaskKey) throws IssueManagementException {
 
+        Map<String, String> issueReleaseMap = new HashMap<String, String>();
+        for (RemoteVersion remoteVersion : issue.getFixVersions()) {
+            String remoteVersionName = remoteVersion.getName();
+            String release = releases.get(remoteVersionName);
+            if (release == null) {
+                try {
+                    release = config.getProperty("jira.version.map.name." + remoteVersionName);
+                } catch (ConfigurationException e) {
+                    release = remoteVersionName;
+                }
+                warnings.add("Version mapping", remoteVersionName + " --> " + release);
+                releases.put(remoteVersionName, release);
+            }
+            issueReleaseMap.put(release, release);
+        }
+        Set<String> issueReleases = issueReleaseMap.keySet();
+        if (issueReleases.size() == 0) {
+            warnings.add(ISSUE_FIELDS, issue.getKey() + " has no fix version");
+        } else if (issueReleases.size() > 1) {
+            warnings.add(ISSUE_FIELDS, issue.getKey() + " has more than one fix version");
+        }
         String typeId = issue.getType();
         String typeName = issueTypes.get(typeId);
         if (typeName == null) {
             typeName = config.getProperty("jira.type.map.id." + typeId, "housekeeping");
-            LOG.info("Mapping raw issue type " + typeId + " to " + typeName);
+            warnings.add("Issue type mapping", typeId + " --> " + typeName);
             issueTypes.put(typeId, typeName);
         }
         Integer weight = issueTypeWeightMultipliers.get(typeName);
@@ -116,7 +145,7 @@ public class JiraService implements IssueManagement {
                 throw new IssueManagementException(
                             "Issue type weight configuration \"" + typeNameKey + "\" is invalid", t);
             }
-            LOG.info("Weight of issue type " + typeName + " is " + weight);
+            warnings.add("Issue type weight", typeName + " = " + weight);
             issueTypeWeightMultipliers.put(typeName, weight);
         }
         return new JiraIssueWrapper(issue, subTaskKey, weight);
@@ -129,7 +158,7 @@ public class JiraService implements IssueManagement {
         if (matcher.matches()) {
             key = matcher.group(1);
         } else {
-            LOG.info("No issue ID found in commit comment: " + commitComment);
+            warnings.add(ISSUE_FIELDS, "No Jira issue key found in commit comment: " + commitComment);
         }
         return key;
     }
