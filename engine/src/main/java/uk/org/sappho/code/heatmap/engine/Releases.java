0cacb6426acd977546d48a89184aff5c59abbfdc
Andrew Heald <andrew@heald.co.uk>
2010-12-07 01:43:18 +0000

    CHM-26: Remove load/save heatmaps. It's better to do this sort of thing with the raw SCM and issue management data.

--- engine/src/main/java/uk/org/sappho/code/heatmap/engine/Releases.java
+++ engine/src/main/java/uk/org/sappho/code/heatmap/engine/Releases.java
@@ -1,19 +1,11 @@
 package uk.org.sappho.code.heatmap.engine;
 
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Reader;
-import java.io.Writer;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Vector;
 
-import org.apache.log4j.Logger;
-
 import com.google.inject.Inject;
-import com.thoughtworks.xstream.XStream;
 
 import uk.org.sappho.code.heatmap.config.Configuration;
 import uk.org.sappho.code.heatmap.config.ConfigurationException;
@@ -23,16 +15,13 @@ import uk.org.sappho.code.heatmap.mapping.HeatMapSelector;
 public class Releases {
 
     private final List<String> releaseNames;
-    private final String storeFilename;
-    private Map<String, HeatMaps> releases = new HashMap<String, HeatMaps>();
+    private final Map<String, HeatMaps> releases = new HashMap<String, HeatMaps>();
     private final HeatMapSelector heatMapSelector;
-    private static final Logger LOG = Logger.getLogger(Releases.class);
 
     @Inject
     public Releases(Configuration config, HeatMapSelector heatMapSelector) throws ConfigurationException {
 
         releaseNames = config.getPropertyList("releases");
-        storeFilename = config.getProperty("releases.store.filename", "heatmap-data.xml");
         this.heatMapSelector = heatMapSelector;
     }
 
@@ -63,24 +52,5 @@ public class Releases {
     public final HeatMaps getHeatMaps(String releaseName) {
 
         return releases.get(releaseName);
-    }
-
-    @SuppressWarnings("unchecked")
-    public void load() throws IOException {
-
-        LOG.info("Loading data from " + storeFilename);
-        XStream xstream = new XStream();
-        Reader reader = new FileReader(storeFilename);
-        releases = (Map<String, HeatMaps>) xstream.fromXML(reader);
-        reader.close();
-    }
-
-    public void save() throws IOException {
-
-        LOG.info("Saving data to " + storeFilename);
-        XStream xstream = new XStream();
-        Writer writer = new FileWriter(storeFilename);
-        xstream.toXML(releases, writer);
-        writer.close();
     }
 }
