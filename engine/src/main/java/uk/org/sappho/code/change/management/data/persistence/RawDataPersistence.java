64444352452e508f3661d5a25c6999549e61b515
mread <matt.read@catlin.com>
2010-12-14 12:20:59 +0000

    CHM-38: add log4j.properties for tests and refactor loader
    CHM-38: refactor to clean up constructors

--- engine/src/main/java/uk/org/sappho/code/change/management/data/persistence/RawDataPersistence.java
+++ engine/src/main/java/uk/org/sappho/code/change/management/data/persistence/RawDataPersistence.java
@@ -1,13 +1,15 @@
 package uk.org.sappho.code.change.management.data.persistence;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.Reader;
 import java.io.Writer;
 
 import org.apache.log4j.Logger;
+
+import com.thoughtworks.xstream.XStream;
 
 import uk.org.sappho.code.change.management.data.IssueData;
 import uk.org.sappho.code.change.management.data.RawData;
@@ -15,61 +17,65 @@ import uk.org.sappho.code.change.management.data.RevisionData;
 import uk.org.sappho.code.change.management.issues.IssueManagementException;
 import uk.org.sappho.configuration.Configuration;
 import uk.org.sappho.configuration.ConfigurationException;
-
-import com.thoughtworks.xstream.XStream;
+import uk.org.sappho.warnings.SimpleWarningList;
 
 public class RawDataPersistence {
 
-	private final XStream xstream = new XStream();
-	private String filename;
-	private static final Logger LOG = Logger.getLogger(RawDataPersistence.class);
-	private InputStream dataInputStream;
+    private final XStream xstream = new XStream();
+    private String filename;
+    private static final Logger LOG = Logger.getLogger(RawDataPersistence.class);
+    private Reader dataReader;
 
-	public RawDataPersistence(String filename) {
+    private RawDataPersistence() {
+        for (Class<?> clazz : new Class[] { RawData.class, IssueData.class, RevisionData.class, SimpleWarningList.class }) {
+            xstream.alias(clazz.getSimpleName(), clazz);
+        }
+    }
 
-		this.filename = filename;
-		init();
-	}
+    public RawDataPersistence(String filename) {
+        this();
+        try {
+            dataReader = new FileReader(filename);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	public RawDataPersistence(Configuration config) throws ConfigurationException {
+    public RawDataPersistence(Configuration config) throws ConfigurationException {
+        this();
+        try {
+            dataReader = new FileReader(config.getProperty("raw.data.store.filename"));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-		filename = config.getProperty("raw.data.store.filename");
-		init();
-	}
+    public RawDataPersistence(Reader reader) {
+        this();
+        this.dataReader = reader;
+    }
 
-	public RawDataPersistence(InputStream dataInputStream) {
-		this.dataInputStream = dataInputStream;
-		init();
-	}
+    public RawData load() throws IOException, IssueManagementException {
 
-	private void init() {
+        LOG.info("Loading data from " + filename);
+        Reader reader = new FileReader(filename);
+        RawData rawData = (RawData) xstream.fromXML(reader);
+        reader.close();
+        return rawData;
+    }
 
-		for (Class<?> clazz : new Class[] { RawData.class, IssueData.class, RevisionData.class }) {
-			xstream.alias(clazz.getSimpleName(), clazz);
-		}
-	}
+    public RawData loadFromInputStream() {
+        LOG.info("Loading data from inputStream");
+        RawData rawData = (RawData) xstream.fromXML(dataReader);
+        return rawData;
+    }
 
-	public RawData load() throws IOException, IssueManagementException {
+    public void save(RawData rawData) throws IOException {
 
-		LOG.info("Loading data from " + filename);
-		Reader reader = new FileReader(filename);
-		RawData rawData = (RawData) xstream.fromXML(reader);
-		reader.close();
-		return rawData;
-	}
-
-	public RawData loadFromInputStream() {
-		LOG.info("Loading data from inputStream");
-		RawData rawData = (RawData) xstream.fromXML(dataInputStream);
-		return rawData;
-	}
-
-	public void save(RawData rawData) throws IOException {
-
-		LOG.info("Saving data to " + filename);
-		Writer writer = new FileWriter(filename);
-		xstream.toXML(rawData, writer);
-		writer.close();
-	}
+        LOG.info("Saving data to " + filename);
+        Writer writer = new FileWriter(filename);
+        xstream.toXML(rawData, writer);
+        writer.close();
+    }
 
 }
--- engine/src/test/java/uk/org/sappho/code/change/management/data/persistence/RawDataPersistenceTest.java
+++ engine/src/test/java/uk/org/sappho/code/change/management/data/persistence/RawDataPersistenceTest.java
@@ -3,7 +3,8 @@ package uk.org.sappho.code.change.management.data.persistence;
 import static org.junit.Assert.assertNotNull;
 
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 
 import org.junit.Test;
 
@@ -12,14 +13,14 @@ import uk.org.sappho.code.change.management.issues.IssueManagementException;
 
 public class RawDataPersistenceTest {
 
-	@Test
-	public void shouldReadInputStream() throws IOException, IssueManagementException {
-		InputStream testData = getClass().getResourceAsStream("raw-data-11.01.xml");
-		RawDataPersistence persistence = new RawDataPersistence(testData);
+    @Test
+    public void shouldReadInputStream() throws IOException, IssueManagementException {
+        Reader testData = new InputStreamReader(getClass().getResourceAsStream("raw-data-11.01.xml"));
+        RawDataPersistence persistence = new RawDataPersistence(testData);
 
-		RawData rawData = persistence.loadFromInputStream();
+        RawData rawData = persistence.loadFromInputStream();
 
-		assertNotNull(rawData);
-	}
+        assertNotNull(rawData);
+    }
 
 }
